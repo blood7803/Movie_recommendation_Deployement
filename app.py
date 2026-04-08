ace80a464cd1ceeeb3598cc00e0f224e6f3040b1
Gaurav Kumar <bloodrag7803@gmail.com>
2026-04-08 07:59:45 +0530

    Add files via upload
    Fixed again

--- app.py
+++ app.py
@@ -34,51 +34,31 @@ import pandas as pd
 import gdown
 import os
 
-# ---------------- DOWNLOAD FILES ---------------- #
+st.write("Starting app...")
 
-def download_file(url, filename):
+def download_file(file_id, filename):
     if not os.path.exists(filename):
-        gdown.download(url, filename, quiet=False)
+        st.write(f"Downloading {filename}...")
+        gdown.download(id=file_id, output=filename, quiet=False)
 
-movie_url = "https://drive.google.com/uc?id=1aLBRrw6IrniFXVZ11UOx92DYcbuqiIql"
-similarity_url = "https://drive.google.com/uc?id=1XNYtGIDlIsodfOk0CgV40T-KrDDygodA"
+movie_id = "1aLBRrw6IrniFXVZ11UOx92DYcbuqiIql"
+similarity_id = "1XNYtGIDlIsodfOk0CgV40T-KrDDygodA"
 
-download_file(movie_url, "movie.pkl")
-download_file(similarity_url, "similarity.pkl")
+download_file(movie_id, "movie.pkl")
+download_file(similarity_id, "similarity.pkl")
 
-# ---------------- LOAD DATA ---------------- #
+# Check file size
+st.write("Movie file size:", os.path.getsize("movie.pkl"))
+st.write("Similarity file size:", os.path.getsize("similarity.pkl"))
+
+# Try loading
+st.write("Loading pickle files...")
 
 movies_dict = pickle.load(open('movie.pkl', 'rb'))
 similarity = pickle.load(open('similarity.pkl', 'rb'))
 
+st.write("Files loaded successfully!")
+
 movies = pd.DataFrame(movies_dict)
 
-# ---------------- UI ---------------- #
-
 st.title('🎬 Movie Recommender System')
-
-def recommend(movie):
-    movie_index = movies[movies['title'] == movie].index[0]
-    distances = similarity[movie_index]
-
-    movie_list = sorted(
-        list(enumerate(distances)),
-        reverse=True,
-        key=lambda x: x[1]
-    )[1:6]
-
-    recommended_movies = []
-    for i in movie_list:
-        recommended_movies.append(movies.iloc[i[0]].title)
-
-    return recommended_movies
-
-selected_movie = st.selectbox(
-    "SELECT MOVIE",
-    movies['title'].values
-)
-
-if st.button('Recommend'):
-    recommendations = recommend(selected_movie)
-    for movie in recommendations:
-        st.write(movie)
